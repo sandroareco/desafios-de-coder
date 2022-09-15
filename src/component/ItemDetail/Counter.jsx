5edfdf50c8eea6237d8956fdb005e986f6328027
sandro1998.areco <sandro1998.areco@gmail.com>
2022-09-14 21:41:48 -0300

    desafio sincronizar counter

--- src/component/ItemDetail/Counter.jsx
+++ src/component/ItemDetail/Counter.jsx
@@ -1,49 +1,42 @@
 import {useState} from 'react';
 import { products } from '../../mock/products';
 
-const Counter = ({stock,title,price,description,initial= 1}) => {
-    const [count] = useState(1)
-    const [cantidad, setCantidad] = useState(1)
+const Counter = ({stock,title,price,description,onAdd, initial= 1}) => {
+    const [count, setCount] = useState(1)
 
-    function buy (products) {
-        const productoCarrito = {id: products.id, cantidad: cantidad}
-        console.log(productoCarrito);
-    }
-
-    const cantidadProducto = (operacion) => {
-        if (operacion === "+") {
-            if(cantidad < products.stock) {
-                setCantidad(cantidad + 1)
-            } else {
-                if (cantidad > initial){
-                setCantidad(cantidad - 1)
-            }
+    function add (){
+        if (count < stock){
+        setCount(count + 1)
         }
     }
 
-    return (
-    <div>
-        <div className='counter-box'>
-             <p className='card-title'> {title}</p>
-             <p className='card-text'>precio:{price}</p>
-             <p className='card-text'>{description}</p>
-             <p className='card-text'> stock:{stock}</p>
-             <p className='card-text'> cantidad: {count}</p>
-            </div>
-            <div> 
-            <button className='btn btn-light' onClick={() => {cantidadProducto("+")}}>
-                +
-            </button>
-            <button className='btn btn-dark' onClick={() => {cantidadProducto("-")}}>
-                -
-            </button>
-            <p>{cantidad}</p>
-            <button className='btn btn-success' onClick={() => buy(products,cantidad)}>añadir al carrito</button>
-        </div>
-    </div>
-    );
+    function substract(){
+        if (count > 0){
+        setCount (count - 1)
+        }
     }
-}
+
+    function reset(){
+        setCount(initial)
+    }
+
+    return (
+        <div>
+            <div className='counter-box'>
+                 <h1>Carrito </h1>
+                 <p>stock: {stock} </p>
+                 <p>Cantidad: {count} </p>
+            <div>  
+                <button className='btn' onClick={substract} > - </button>
+                <button className='btn' onClick={reset} > Reset</button>
+                <button className='btn' onClick={add} > + </button>
+            </div>
+                <button className='btn' onClick={() => onAdd(count)}>añadir al carrito</button>
+            </div>
+        </div>
+           );
+    }
+
 
 export default Counter;
 
@@ -62,37 +55,49 @@ export default Counter;
 
 
 
-    // function add (){
-    //     if (count < stock){
-    //     setCount(count + 1)
-    //     }
-    // }
-
-    // function substract(){
-    //     if (count > 0){
-    //     setCount (count - 1)
-    //     }
-    // }
-
-    // function reset(){
-    //     setCount(0)
-    // }
-
-    // return (
-    //     <div>
-    //         <div className='counter-box'>
-    //              <h1>Carrito </h1>
-    //              <p>stock: {stock} </p>
-    //              <p>Cantidad: {count} </p>
-    //         <div>  
-    //             <button className='btn' onClick={substract} > - </button>
-    //             <button className='btn' onClick={reset} > Reset</button>
-    //             <button className='btn' onClick={add} > + </button>
-    //         </div>
-    //             <button className='btn' onClick={() => onAdd(count)}>añadir al carrito</button>
-    //         </div>
-    //     </div>
-    //        );
-    // }
 
    
+
+
+
+    // const [cantidad, setCantidad] = useState(1)
+
+    // function buy (products) {
+    //     const productoCarrito = {id: products.id, cantidad: cantidad}
+    //     console.log(productoCarrito);
+    // }
+
+    // const cantidadProducto = (operacion) => {
+    //     if (operacion === "+") {
+    //         if(cantidad < products.stock) {
+    //             setCantidad(cantidad + 1)
+    //         } else {
+    //             if (cantidad > initial){
+    //             setCantidad(cantidad - 1)
+    //         }
+    //     }
+    // }
+
+
+//     <div>
+//     <div className='counter-box'>
+//          <p className='card-title'> {title}</p>
+//          <p className='card-text'>precio:{price}</p>
+//          <p className='card-text'>{description}</p>
+//          <p className='card-text'> stock:{stock}</p>
+//          <p className='card-text'> cantidad: {count}</p>
+//         </div>
+//         <div> 
+//         <button className='btn btn-light' onClick={() => {cantidadProducto("+")}}>
+//             +
+//         </button>
+//         <button className='btn btn-dark' onClick={() => {cantidadProducto("-")}}>
+//             -
+//         </button>
+//         <p>{cantidad}</p>
+//         <button className='btn btn-success' onClick={() => buy(products,cantidad)}>añadir al carrito</button>
+//     </div>
+// </div>
+// );
+// }
+// }
